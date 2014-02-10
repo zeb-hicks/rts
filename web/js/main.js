31df7a4fddca3f186598a0b4b13cf9c117935332
Zeb Hicks <Nimphious@gmail.com>
2014-02-11 02:26:08 +1100

    Cleanup
    Cleaning up old pre-loader script.

--- web/js/main.js
+++ web/js/main.js
@@ -1,19 +1,5 @@
 window.requestAnimationFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame;
 
-// Basic javascript imports.
-// Should probably use something more sophisticated but this will do for now.
-
-// var importlist = {};
-// function imports(js, cb) {
-// 	var scr = document.createElement('script');
-// 	scr.src = './js/' + js;
-// 	importlist[js] = false;
-// 	scr.addEventListener('load', function() {
-// 		importlist[js] = true;
-// 	});
-// 	document.head.appendChild(scr);
-// }
-
 function importAll() {
 	var list = [];
 	for (var i = 0; i < arguments.length; i++) list.push(arguments[i]);
@@ -38,23 +24,6 @@ function loadNext(list, cb) {
 	document.head.appendChild(scr);
 }
 
-// Brute force import load checking.
-// var icheck = setInterval(function() {
-// 	var ii = 0;
-// 	var ic = 0;
-// 	for (var a in importlist) {
-// 		ii++;
-// 		if (importlist[a]) {
-// 			delete importlist[a] == true;
-// 			ic++;
-// 		}
-// 	}
-// 	if (ic > 0 && ic == ii) {
-// 		clearInterval(icheck);
-// 		init();
-// 	}
-// }, 100);
-
 // Synchronous XHR function. Will have to refactor to be async later on. For now this simplifies some things.
 function loadFile(f) {
 	var xhr = new XMLHttpRequest();
@@ -62,16 +31,6 @@ function loadFile(f) {
 	xhr.send(null);
 	return xhr.responseText;
 }
-
-// imports('lib/glowcore.js');
-// imports('lib/glowexts.js');
-
-// imports('loop.js');
-// imports('draw.js');
-
-// imports('world.js');
-// imports('planet.js');
-// imports('game.js');
 
 importAll('lib/glowcore.js', 'lib/glowexts.js', 'loop.js', 'draw.js', 'world.js', 'planet.js', 'game.js', init);
 
